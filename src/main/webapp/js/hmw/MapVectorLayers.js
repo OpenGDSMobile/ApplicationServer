25aa74486765616a471c54b08402efb1dd8b1e82
engintruder <engkwangseob@gmail.com>
2014-10-28 20:21:43 +0900

    openlayer style Start

--- src/main/webapp/js/hmw/MapVectorLayers.js
+++ src/main/webapp/js/hmw/MapVectorLayers.js
@@ -13,98 +13,55 @@ var	LayerSources =null;
 	 */
 Layer.createLayer = function(obj,color,width){
 	obj  = $(obj).attr('data-id'); 
-	extent = Map.map.getView().calculateExtent(Map.map.getSize());
-	console.log(extent);
-	parseResponse = function(features){
-		console.log(features);
-		var canvasFunction = function(extent,resolution,pixelRatio,size,projection){
-			var canvasWidth = size[0];
-		    var canvasHeight = size[1];
-		    console.log(extent);
-		    var canvas = d3.select(document.createElement('canvas'));
-		    canvas.attr('width', canvasWidth).attr('height', canvasHeight);
-
-		    var context = canvas.node().getContext('2d');
-
-		    var d3Projection = d3.geo.mercator().scale(1).translate([0, 0]);
-		    var d3Path = d3.geo.path().projection(d3Projection);
-
-		    var pixelBounds = d3Path.bounds(features);
-		    var pixelBoundsWidth = pixelBounds[1][0] - pixelBounds[0][0];
-		    var pixelBoundsHeight = pixelBounds[1][1] - pixelBounds[0][1];
-
-		    var geoBounds = d3.geo.bounds(features);
-		    var geoBoundsLeftBottom = ol.proj.transform(
-		        geoBounds[0], 'EPSG:4326', projection);
-		    var geoBoundsRightTop = ol.proj.transform(
-		        geoBounds[1], 'EPSG:4326', projection);
-		    var geoBoundsWidth = geoBoundsRightTop[0] - geoBoundsLeftBottom[0];
-		    if (geoBoundsWidth < 0) {
-		      geoBoundsWidth += ol.extent.getWidth(projection.getExtent());
-		    }
-		    var geoBoundsHeight = geoBoundsRightTop[1] - geoBoundsLeftBottom[1];
-
-		    var widthResolution = geoBoundsWidth / pixelBoundsWidth;
-		    var heightResolution = geoBoundsHeight / pixelBoundsHeight;
-		    var r = Math.max(widthResolution, heightResolution);
-		    var scale = r / (resolution / pixelRatio);
-
-		    var center = ol.proj.transform(ol.extent.getCenter(extent),
-		        projection, 'EPSG:4326');
-		    d3Projection.scale(scale).center(center)
-		        .translate([canvasWidth / 2, canvasHeight / 2]);
-		    d3Path = d3Path.projection(d3Projection).context(context);
-		    //d3Path(features); 
-		    $.each(features.features, function(index, value){ 
-		    	
-		    	console.log(value.properties.SIG_KOR_NM.toString());
-		    	var test = new String('은평구');
-		    	if(value.properties.SIG_KOR_NM.toString()=='강동구'){
-		    		console.log("eee");
-		    		d3Path(value); 
-			        context.fillStyle = '#8ED6FF';
-			        context.fill();
-			    	context.strokeStyle = '#ff0000';
-			    	context.stroke(); 
-		    	}
-		    	else{
-		    		console.log("eee1");
-		    		d3Path(value); 
-		    		context.stroke();
-		    	}
-		    });
-		    //d3Path(features.features[0]); 
-		    return canvas[0][0];
-		};
-		var layer = new ol.layer.Image({
-		    source: new ol.source.ImageCanvas({
-		      canvasFunction: canvasFunction,
-		      projection: 'EPSG:900913'
-		    })
-		  });
-		Map.map.addLayer(layer);
-	};
-	var url = 'http://113.198.80.9/geoserver/wfs';
-	//var url = 'http://113.198.80.9/geoserver/wfs?service=wfs&version=1.1.0&request=GetFeature&typeNames=opengds:seoul_si&outputFormat=text/javascript';
-	$.ajax({
-		type:'GET',
-		url:url, 
-		data : {
-			service : 'wfs',
-			version : '1.1.0',
-			request : 'getFeature',
-			typeNames : 'opengds:'+obj,
-			outputFormat : 'text/javascript'
+	Layer.vectorSource = new ol.source.ServerVector({
+		format: new ol.format.GeoJSON(),
+		loader: function(extent, resolution, projection){
+			//console.log('Loading Data: '+data);
+			var url = 'http://113.198.80.9/geoserver/wfs?service=WFS&' +
+			'version=1.1.0&request=GetFeature' +  
+			'&typeNames=opengds:'+obj+ 
+			'&outputFormat=text/javascript&format_options=callback:loadFeatures' +
+			'&srsname=EPSG:900913&bbox=' + extent.join(',') + ',EPSG:900913'; 
+			$.ajax({ 
+				url : url,
+				dataType: 'jsonp' 
+			});  
 		},
-		dataType : 'jsonp',
-		jsonpCallback : 'parseResponse',
-		success:function(msg){ 
-			console.log("OK"); 
-		},
-		error:function(xhr, status , err){
-			console.log(status +';'+err);
-		}
-	});   
+		strategy: ol.loadingstrategy.createTile(new ol.tilegrid.XYZ({
+			maxZoom: 19
+		})),
+		projection: 'EPSG:900913'
+	}); 
+	loadFeatures = function(response){   
+		Layer.vectorSource.addFeatures(Layer.vectorSource.readFeatures(response));  
+	}; 
+	
+	var styleArray = [new ol.style.Style({
+		  fill: new ol.style.Fill({
+		    color: 'rgba(255, 255, 255, 0.6)'
+		  }),
+		  stroke: new ol.style.Stroke({
+		    color: '#319FD3',
+		    width: 1
+		  })
+		})];
+	var vectorTemp = new ol.layer.Vector({
+    	title:obj,
+ 	   	source: Layer.vectorSource,
+ 	   	style: function(f,r){ 
+ 	   		console.log(f.get('SIG_KOR_NM'));
+ 	   		return styleArray;
+ 	   	}
+ 	   	/*style: new ol.style.Style({
+ 		   stroke: new ol.style.Stroke({
+ 			   color:color,
+ 			   width:width
+ 		   })
+ 	   })
+ 	   */
+    });
+    Map.map.addLayer(vectorTemp); 
+	
 };
 
 Layer.removeLayer = function(data){
--- src/main/webapp/js/hmw/tmp.js
+++ src/main/webapp/js/hmw/tmp.js
@@ -1,4 +1,98 @@
 
+	extent = Map.map.getView().calculateExtent(Map.map.getSize());
+	console.log(extent);
+	parseResponse = function(features){
+		console.log(features);
+		var canvasFunction = function(extent,resolution,pixelRatio,size,projection){
+			var canvasWidth = size[0];
+		    var canvasHeight = size[1];
+		    console.log(extent);
+		    var canvas = d3.select(document.createElement('canvas'));
+		    canvas.attr('width', canvasWidth).attr('height', canvasHeight);
+
+		    var context = canvas.node().getContext('2d');
+
+		    var d3Projection = d3.geo.mercator().scale(1).translate([0, 0]);
+		    var d3Path = d3.geo.path().projection(d3Projection);
+
+		    var pixelBounds = d3Path.bounds(features);
+		    var pixelBoundsWidth = pixelBounds[1][0] - pixelBounds[0][0];
+		    var pixelBoundsHeight = pixelBounds[1][1] - pixelBounds[0][1];
+
+		    var geoBounds = d3.geo.bounds(features);
+		    var geoBoundsLeftBottom = ol.proj.transform(
+		        geoBounds[0], 'EPSG:4326', projection);
+		    var geoBoundsRightTop = ol.proj.transform(
+		        geoBounds[1], 'EPSG:4326', projection);
+		    var geoBoundsWidth = geoBoundsRightTop[0] - geoBoundsLeftBottom[0];
+		    if (geoBoundsWidth < 0) {
+		      geoBoundsWidth += ol.extent.getWidth(projection.getExtent());
+		    }
+		    var geoBoundsHeight = geoBoundsRightTop[1] - geoBoundsLeftBottom[1];
+
+		    var widthResolution = geoBoundsWidth / pixelBoundsWidth;
+		    var heightResolution = geoBoundsHeight / pixelBoundsHeight;
+		    var r = Math.max(widthResolution, heightResolution);
+		    var scale = r / (resolution / pixelRatio);
+
+		    var center = ol.proj.transform(ol.extent.getCenter(extent),
+		        projection, 'EPSG:4326');
+		    d3Projection.scale(scale).center(center)
+		        .translate([canvasWidth / 2, canvasHeight / 2]);
+		    d3Path = d3Path.projection(d3Projection).context(context);
+		    //d3Path(features); 
+		    $.each(features.features, function(index, value){ 
+		    	
+		    	console.log(value.properties.SIG_KOR_NM.toString());
+		    	var test = new String('����');
+		    	if(value.properties.SIG_KOR_NM.toString()=='������'){
+		    		console.log("eee");
+		    		d3Path(value); 
+			        context.fillStyle = '#8ED6FF';
+			        context.fill();
+			    	context.strokeStyle = '#ff0000';
+			    	context.stroke(); 
+		    	}
+		    	else{
+		    		console.log("eee1");
+		    		d3Path(value); 
+		    		context.stroke();
+		    	}
+		    });
+		    //d3Path(features.features[0]); 
+		    return canvas[0][0];
+		};
+		var layer = new ol.layer.Image({
+		    source: new ol.source.ImageCanvas({
+		      canvasFunction: canvasFunction,
+		      projection: 'EPSG:900913'
+		    })
+		  });
+		Map.map.addLayer(layer);
+	};
+	var url = 'http://113.198.80.9/geoserver/wfs';
+	//var url = 'http://113.198.80.9/geoserver/wfs?service=wfs&version=1.1.0&request=GetFeature&typeNames=opengds:seoul_si&outputFormat=text/javascript';
+	$.ajax({
+		type:'GET',
+		url:url, 
+		data : {
+			service : 'wfs',
+			version : '1.1.0',
+			request : 'getFeature',
+			typeNames : 'opengds:'+obj,
+			outputFormat : 'text/javascript'
+		},
+		dataType : 'jsonp',
+		jsonpCallback : 'parseResponse',
+		success:function(msg){ 
+			console.log("OK"); 
+		},
+		error:function(xhr, status , err){
+			console.log(status +';'+err);
+		}
+	});   
+
+
 	/*
 	
 	Layer.vectorSource = new ol.source.ServerVector({
