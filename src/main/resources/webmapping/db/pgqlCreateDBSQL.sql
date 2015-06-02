9b21320aecc3595aa5522ba6240eba1753517ae7
engintruder <engkwangseob@gmail.com>
2015-06-02 13:57:42 +0900

    Modify : postgresql db + mybatis  properties (eGovframework mobile common components compatible)

---
+++ src/main/resources/webmapping/db/pgqlCreateDBSQL.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE OPENGDSMOBILE_DB
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
