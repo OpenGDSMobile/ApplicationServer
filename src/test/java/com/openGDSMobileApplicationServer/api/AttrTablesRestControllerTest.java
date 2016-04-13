1671160316c00136ee62802bb1fac566537fd2b9
engintruder <engkwangseob@gmail.com>
2016-04-14 02:35:19 +0900

    code coverage draft

--- src/test/java/com/openGDSMobileApplicationServer/api/AttrTablesRestControllerTest.java
+++ src/test/java/com/openGDSMobileApplicationServer/api/AttrTablesRestControllerTest.java
@@ -23,6 +23,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
